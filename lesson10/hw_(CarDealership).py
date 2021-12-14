8453685cdcc244f7c0dfa889dc4d929c4a903498
semaleksey <sembublik@gmail.com>
2021-12-14 18:40:55 +0400

    add lesson10

---
+++ lesson10/hw_(CarDealership).py
@@ -0,0 +1,25 @@
+from datetime import date
+
+
+class CarDealership:
+    pass
+
+
+class Car:
+    def __init__(self, id: str, model: str, country_of_manufacture: str,
+                 date_of_manufacture: date, engine: str, price: str):
+        self.id = id
+        self.model = model
+        self.country_of_manufacture = country_of_manufacture
+        self.date_of_manufacture = date_of_manufacture
+        self.engine = engine
+        self.price = price
+
+
+class Lorry(Car):
+    def __init__(self, id: str, model: str, country_of_manufacture: str,
+                 date_of_manufacture: date, engine: str, price: str,
+                 maximum_lifting_capacity: str):
+        super().__init__(self, id, model, country_of_manufacture,
+                         date_of_manufacture, engine, price)
+        self.maximum_lifting_capacity = maximum_lifting_capacity
